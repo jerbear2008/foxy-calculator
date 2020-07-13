f9f5e5e6a124b9e6485ee7817af5a555626182be
paulchrisycj <paulchrisycj@gmail.com>
2020-07-14 00:12:19 +0800

    Fix bug and show bracket when using some equations

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,27 +1,27 @@
 $('#cos').click(function(e){
-    $('#calcInput').val("cos");
+    $('#calcInput').val($('#calcInput').val() + "cos(");
 })
 $('#sin').click(function(e){
-    $('#calcInput').val("sin");
+    $('#calcInput').val($('#calcInput').val() + "sin(");
 })
 $('#tan').click(function(e){
-    $('#calcInput').val("tan");
+    $('#calcInput').val($('#calcInput').val() + "tan(");
 })
 $('#pi').click(function(e){
-    $('#calcInput').val("pi");
+    $('#calcInput').val($('#calcInput').val() + "pi(");
 })
 $('#sqrt').click(function(e){
-    $('#calcInput').val("sqrt");
+    $('#calcInput').val($('#calcInput').val() + "sqrt(");
 })
 $('#square').click(function(e){
-    $('#calcInput').val("^2");
+    $('#calcInput').val($('#calcInput').val() + "^2");
 })
 $('#deg').click(function(e){
-    $('#calcInput').val("degree");
+    $('#calcInput').val($('#calcInput').val() + "degrees(");
 })
 $('#rad').click(function(e){
-    $('#calcInput').val("radian");
+    $('#calcInput').val($('#calcInput').val() + "radian");
 })
 $('#fac').click(function(e){
-    $('#calcInput').val("!");
+    $('#calcInput').val($('#calcInput').val() + "!");
 })
